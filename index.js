9c7ac6b2860aa1693aa45bfa4742a1e76188bd46
Doguhan Uluca <duluca@gmail.com>
2016-01-16 11:42:03 -0500

    updated array

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 5000;
 
 app.post('/', function (req, res) {
     var result = {
-        data: [1,2,3,5,8,13]
+        data: [1,1,2,3,5,8,13]
     }
 
     res.send(result);

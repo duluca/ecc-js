1a881bab0d57b521e3181b2992e1c5d1d8ea90db
Doguhan Uluca <duluca@gmail.com>
2016-01-30 14:55:56 -0500

    trying...

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-/**
- * Created by doguhanuluca on 1/16/16.
- */
-'use strict';
-
-var express = require('express');
+var express = require("express");
 var app = express();
+var request = require("request");
+var bodyParser = require("body-parser");
 
-var port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/', function (req, res) {
-    var result = {
-        data: [1,1,2,3,5,8,13]
-    }
+try{
+  var env = require("./env.json");
+}catch(e){
+  var env = process.env;
+}
 
-    res.send(result);
+var stations;
+
+(function setStations(){
+  api("jLines", {}, function(data){
+    stations = data;
+  });
+}());
+
+app.get("/", function(req, res){
+  res.json({success: true});
 });
 
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port);
-})
+app.post("/", function(req, res){
+  var body = req.body;
+  var params = {
+    Lat: (body.latitude || null),
+    Lon: (body.longitude || null),
+    Radius: (body.radius || null)
+  }
+  api("jStationEntrances", params, getStationEntrances);
+
+  function getStationEntrances(data){
+    api("jStationInfo", {StationCode: data.Entrances[0]["StationCode1"]}, function(dat){
+      res.send({
+        station: dat.Name,
+        stationLat: dat.Lat,
+        stationLong: Number(dat.Lon.toFixed(6))
+      });
+    });
+  }
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Listening!");
+});
+
+
+function api(endpoint, params, callback){
+  request({
+    uri: "https://api.wmata.com/Rail.svc/json/" + endpoint,
+    method: "GET",
+    qs: (params || {}),
+    headers: {
+      "Content-Type": "application/json",
+      "api_key": '690d19a27b92490cbcf79f679dd423e4'
+    }
+  }, function(err, res){
+    callback(JSON.parse(res.body), res);
+  });
+}
